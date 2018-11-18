6be8510b986b43d3d74fea710e9c682473a0ee10
Abhilash Jha <abhilash@nexthoughts.com>
2018-11-19 00:34:16 +0530

    Updated database configurations

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -3,7 +3,7 @@ dataSource {
     jmxExport = true
     driverClassName = "com.mysql.jdbc.Driver"
     username = "root"
-    password = "12345"
+    password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
