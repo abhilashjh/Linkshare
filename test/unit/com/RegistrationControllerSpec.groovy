89a716685b2baf741709213510fea1ddcf18d986
Abhilash Jha <abhilash@nexthoughts.com>
2018-11-19 13:02:47 +0530

    Created unit test

--- test/unit/com/RegistrationControllerSpec.groovy
+++ test/unit/com/RegistrationControllerSpec.groovy
@@ -1,5 +1,6 @@
 package com
 
+import com.security.User
 import grails.test.mixin.TestFor
 import preLogin.RegistrationController
 import spock.lang.Specification
@@ -11,11 +12,13 @@ import spock.lang.Specification
 class RegistrationControllerSpec extends Specification {
 
     def setup() {
-    }
+        new User(username: 'abhilash', password: '123456', email: 'abhilashjha@gmail.com', firstName: 'Abhilash', lastName: 'Jha', admin: Boolean.FALSE).save(flush: true)    }
 
     def cleanup() {
     }
 
     void "test something"() {
+        expect:
+            User.count() == 1
     }
 }
--- travis-build.sh
+++ travis-build.sh
@@ -5,5 +5,7 @@ EXIT_STATUS=0
 
 ./grailsw refresh-dependencies --non-interactive
 ./grailsw compile --non-interactive
+./grailsw test-app :unit --non-interactive
+./grailsw war --non-interactive
 
 exit $EXIT_STATUS
