9065881cbdbe34f5f8912749a416392ac34083be
Abhilash Jha <abhilash@nexthoughts.com>
2018-11-23 23:16:38 +0530

    Changes made to deploy script

--- travis-build.sh
+++ travis-build.sh
@@ -8,4 +8,8 @@ EXIT_STATUS=0
 ./grailsw test-app :unit --non-interactive
 ./grailsw war --non-interactive
 
+zip -r target *
+mkdir -p dpl_cd_upload
+mv target.zip dpl_cd_upload/target.zip
+
 exit $EXIT_STATUS
